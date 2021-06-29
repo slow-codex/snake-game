4c3e0acaced96f73d323d84db6d4c72d6cd02b07
Arnab Dutta <arnabduttasocial@gmail.com>
2021-06-30 00:19:21 +0530

    Updates

---
+++ precache-manifest.6bf7361686d4d46c6e8e250e3a715b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43b6f76305082fb99094",
+    "url": "/snake-game/static/css/main.b219c5ab.chunk.css"
+  },
+  {
+    "revision": "43b6f76305082fb99094",
+    "url": "/snake-game/static/js/main.d5edb33a.chunk.js"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  },
+  {
+    "revision": "4c58baf1482144cc0c26",
+    "url": "/snake-game/static/js/2.ef5a9ea7.chunk.js"
+  },
+  {
+    "revision": "30cac9ba0851a95271b4062bfcc4fc93",
+    "url": "/snake-game/index.html"
+  }
+];
